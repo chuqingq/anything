a74ccec010981b7cc2d9b2665165132cb6a91945
chuqq <chuqingq@qq.com>
2022-04-01 20:50:45 +0800

    删除无用注释

--- anything.go
+++ anything.go
@@ -3,7 +3,6 @@ package main
 import (
 	"io/fs"
 	"log"
-	"os"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -11,10 +10,6 @@ import (
 
 	"github.com/rjeczalik/notify"
 )
-
-// TODO request：db更新后，检查结果是否有更新，如果有，更新结果
-// TODO 监视文件：
-// TODO close
 
 type Watcher struct {
 	dirs       []string              // 需要看护的多个路径。绝对路径，目录
@@ -43,9 +38,9 @@ func (w *Watcher) Close() {
 	for _, s := range w.watches {
 		close(s.outputChan)
 	}
-	os.Remove(dbFile)
 }
 
+// Search 一次性查询
 func (w *Watcher) Search(pattern string) []File {
 	// TODO
 	// 1. searchCache
@@ -59,6 +54,7 @@ func (w *Watcher) Search(pattern string) []File {
 	return output
 }
 
+// Watch 观察。结果有变化时会通过channel返回
 func (w *Watcher) Watch(pattern string) chan *[]File {
 	patterns := parsePattern(pattern)
 	if patterns == nil {
@@ -74,10 +70,10 @@ func (w *Watcher) Watch(pattern string) chan *[]File {
 	return search.outputChan
 }
 
-// StopWatch 应该loop协程来close chan
+// StopWatch 停止观察。这里只发送停止请求，由内部协程处理。
 func (w *Watcher) StopWatch(outputChan chan *[]File) {
 	watch := &Watch{
-		// patterns == nil 用于关闭watch
+		// 约定：patterns == nil 用于关闭watch
 		outputChan: outputChan,
 	}
 	w.watchChan <- watch
@@ -93,18 +89,28 @@ type File struct {
 }
 
 type Watch struct {
-	pattern    string
+	pattern    string // TODO 后续用于作为缓存的key
 	patterns   []string
-	output     []File
+	output     []File // TODO 可以不要
 	outputChan chan *[]File
 }
 
 func parsePattern(pattern string) []string {
+	// 转小写、去空白、分割
 	patterns := strings.Fields(strings.TrimSpace(strings.ToLower(pattern)))
 	// if len(patterns) == 0 {
 	// 	return nil
 	// }
-	// TODO 还要去重
+	// 去重
+	m := map[string]struct{}{}
+	for _, p := range patterns {
+		m[p] = struct{}{}
+	}
+	patterns = patterns[:0]
+	for k := range m {
+		patterns = append(patterns, k)
+	}
+	// 排序
 	sort.StringSlice(patterns).Sort()
 	return patterns
 }
@@ -189,178 +195,13 @@ func isWatchAffectedByEvents(w *Watch, events []notify.EventInfo) bool {
 	return false
 }
 
-// func (w *Watcher) handleNotifyEvent(event notify.EventInfo) {
-// 	w.events = append(w.events, event)
-// 	// // TODO 暂时忽略event，刷新path
-// 	// files, index, found := w.findFiles(path)
-// 	// if files == nil {
-// 	// 	log.Printf("ERROR findFiles(%v) error, unexpected", path)
-// 	// 	return
-// 	// }
-// 	// _, err := os.Lstat(event.Path())
-// 	// // 如果不存在，应该删除
-// 	// if os.IsNotExist(err) {
-// 	// 	// TODO files中删除index
-// 	// 	if index < 0 || index >= len(*files) {
-// 	// 		log.Printf("ERROR index invalid: %v, len: %v, 删除的文件，但是db中没有，unexpected", len(*files), index)
-// 	// 		return
-// 	// 	}
-// 	// 	*files = append((*files)[0:index], (*files)[index+1:]...)
-// 	// 	return
-// 	// }
-// 	// if err != nil {
-// 	// 	log.Printf("ERROR lstate(%v) error: %v", path, err)
-// 	// 	return
-// 	// }
-// 	// // 如果找到了，就更新该File
-// 	// if found {
-// 	// 	log.Printf("ERROR 找到了File. unexpected")
-// 	// 	// update("", (*files)[index], fileInfo)
-// 	// } else {
-// 	// 	// 如果没找到，需要添加
-// 	// 	_, file := filepath.Split(path)
-// 	// 	f := &File{
-// 	// 		Name: file,
-// 	// 		// Dir:  dir,
-// 	// 	}
-// 	// 	// *files = append((*files)[0:index], f, (*files)[index:]...)
-// 	// 	*files = append(*files, nil)               // 切片扩展1个空间
-// 	// 	copy((*files)[index+1:], (*files)[index:]) // a[i:]向后移动1个位置
-// 	// 	(*files)[index] = f                        // 设置新添加的元素
-// 	// 	// update("", f, fileInfo)
-// 	// }
-
-// }
-
-// result1: path应当所属的files
-// result2: path应该当在files中的位置
-// resutl3: result2是否就是path对应的file。如果为false，可能是需要在result2处新增file
-// func (w *Watcher) findFiles(path string) (*[]*File, int, bool) {
-// 	// TODO
-// 	for _, f := range w.files {
-// 		fabs := f.Path // .Join(f.Dir, f.Name)
-// 		if strings.HasPrefix(path, fabs) {
-// 			suffix := strings.TrimPrefix(path, fabs)
-// 			dirs := strings.Split(strings.Trim(suffix, string(os.PathSeparator)), string(os.PathSeparator))
-// 			r, ind, found := findFiles(f, dirs)
-// 			return r, ind, found
-// 		}
-// 	}
-// 	return nil, 0, false
-// }
-
-// func findFiles(f File, dirs []string) (*[]*File, int, bool) {
-// 	// // log.Printf("findFiles: f:%v, dirs: %v", f.Name, dirs)
-// 	// if len(dirs) == 0 {
-// 	// 	log.Printf("ERROR findFiles len(dir) == 0, unexpected")
-// 	// 	return nil, 0, false
-// 	// }
-// 	// if len(dirs) == 1 {
-// 	// 	for i, c := range f.Files {
-// 	// 		r := strings.Compare(dirs[0], c.Name)
-// 	// 		if r == 0 {
-// 	// 			return &f.Files, i, true
-// 	// 		} else if r < 0 {
-// 	// 			return &f.Files, i, false
-// 	// 		}
-// 	// 	}
-// 	// 	return &f.Files, len(f.Files), false
-// 	// }
-// 	// //
-// 	// for _, c := range f.Files {
-// 	// 	if f.Name == c.Name {
-// 	// 		fs, index, found := findFiles(c, dirs[1:])
-// 	// 		return fs, index, found
-// 	// 	}
-// 	// }
-// 	// log.Printf("ERROR findFiles dirs[0](%v) not found, unexpected", dirs[0])
-// 	return nil, 0, false
-// }
-
-// const INDENT = "  "
-
-// func (w *Watcher) generate() {
-// 	w.files = make([]*File, len(w.dirs), len(w.dirs))
-// 	for i := 0; i < len(w.dirs); i++ {
-// 		dir, base := filepath.Split(w.dirs[i])
-// 		// log.Printf("load: %v, %v", dir, base)
-// 		w.files[i] = &File{
-// 			// NameAbs:   db.dirs[i],
-// 			Name: base,
-// 			// NameLower: strings.ToLower(base),
-// 			Dir: dir,
-// 		}
-// 	}
-
-// 	for _, f := range w.files {
-// 		update("", f, nil)
-// 	}
-// 	// log.Printf("generate success; enter handleSearch")
-// 	// // 给search返回结果
-// 	// for _, s := range w.searches {
-// 	// 	w.handleSearch(s)
-// 	// }
-// 	// // debug
-// 	// writeDB(w.files)
-// }
-
-// // 全量刷新
-// func update(indent string, f *File, fileInfo fs.FileInfo) {
-// 	// log.Printf("#%v%v", indent, fileInfo.Name())
-// 	if fileInfo == nil {
-// 		var err error
-// 		fileInfo, err = os.Lstat(filepath.Join(f.Dir, f.Name)) // f.NameAbs
-// 		if err != nil {
-// 			log.Printf("ERROR lstat error: %v", err)
-// 			return
-// 		}
-// 	}
-// 	// 1.更新File本身
-// 	// f.NameAbs在上级更新
-// 	f.Name = fileInfo.Name()
-// 	f.NameLower = strings.ToLower(f.Name)
-// 	// TODO f.Type
-// 	f.Size = fileInfo.Size()
-// 	// f.Dir在上级更新
-// 	f.ModifyTime = fileInfo.ModTime()
-// 	// 2.更新f.Files
-// 	if !fileInfo.IsDir() {
-// 		f.Files = nil
-// 		// log.Printf("file refreshed")
-// 		return
-// 	}
-// 	// log.Printf("###isdir: %v", f.NameAbs)
-// 	dirEntrys, err := os.ReadDir(filepath.Join(f.Dir, f.Name)) // f.NameAbs
-// 	if err != nil {
-// 		log.Printf("ERROR readdir error: %v", err)
-// 		return
-// 	}
-// 	f.Files = make([]*File, 0)
-// 	for _, d := range dirEntrys {
-// 		// log.Printf("%v ==> %v", f.NameAbs, d.Name())
-// 		fi, err := d.Info()
-// 		if err != nil {
-// 			log.Printf("ERROR: d.Info() error: %v", err)
-// 			continue // TODO
-// 		}
-// 		newf := &File{
-// 			// NameAbs: filepath.Join(f.Dir, f.Name, d.Name()),
-// 			Name: d.Name(),
-// 			Dir:  filepath.Join(f.Dir, f.Name),
-// 		}
-// 		f.Files = append(f.Files, newf)
-// 		update(indent+INDENT, newf, fi)
-// 	}
-// }
-
-func (w *Watcher) handleWatch(s *Watch) {
-	log.Printf("handleSearch(%v)", s.pattern)
-
+func (w *Watcher) handleWatch(watch *Watch) {
+	log.Printf("handleSearch(%v)", watch.pattern)
 	// 执行search请求
-	search(s.patterns, w.files, &s.output) // TODO 未优化
+	search(watch.patterns, w.files, &watch.output) // TODO 未优化
 	// 发送search响应。不能阻塞
 	select {
-	case s.outputChan <- &s.output:
+	case watch.outputChan <- &watch.output:
 	default:
 		log.Printf("ERROR output error")
 	}
