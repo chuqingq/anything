f76fa872d87bc106af6a04f42e342dbfdd6e1024
chuqq <chuqingq@qq.com>
2022-04-18 15:52:18 +0800

    显示dir和file

--- ui.go
+++ ui.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -11,14 +11,65 @@ import (
 )
 
 // go build -ldflags "-H windowsgui -s -w"
+
+// TODO
+// - 自动最大化
+// - table各列的宽度能够设置
+// - 输入框支持中文
+// - open和openfolder的实现
+// - 按键自动focus到entry？
+
 func main() {
+	// watcher
+	dirs := []string{"C:\\Program Files\\Go"} //
+	watcher := &Watcher{
+		dirs: dirs,
+	}
+	watcher.generate()
+
+	// data
+	req := ""
+	var files []File
+
+	// ui
 	a := app.New()
 	w := a.NewWindow("Anything")
 	// entry
 	entry := widget.NewEntry()
 	entry.PlaceHolder = "Search file here"
+
+	// table
+	t := widget.NewTable(
+		func() (int, int) { return 20, 4 },
+		func() fyne.CanvasObject {
+			return widget.NewLabel("      ")
+		},
+		func(id widget.TableCellID, cell fyne.CanvasObject) {
+			dir := ""
+			file := ""
+			if id.Row < len(files) {
+				dir, file = filepath.Split(files[id.Row].Path)
+			}
+			// set content
+			label := cell.(*widget.Label)
+			switch id.Col {
+			case 0:
+				// label.SetText(fmt.Sprintf("%d", id.Row+1))
+				label.SetText(file)
+			case 1:
+				label.SetText(dir)
+			default:
+				// label.SetText(fmt.Sprintf("Cell %d, %d", id.Row+1, id.Col+1))
+				label.SetText("")
+			}
+		})
+	// entry
 	entry.OnChanged = func(s string) {
-		log.Printf("OnChanged: %v", s)
+		req = s
+		// TODO 不能每次都刷新
+		files = watcher.Search(req)
+		log.Printf("OnChanged: %v: %v", s, len(files))
+		t.Refresh()
 	}
 	entry.OnSubmitted = func(s string) {
 		log.Printf("OnSubmitted: %v", s)
@@ -31,25 +82,8 @@ func main() {
 	openFolder := widget.NewButton("Open Folder", func() {
 		log.Printf("open folder")
 	})
-	// table
-	t := widget.NewTable(
-		func() (int, int) { return 10, 4 },
-		func() fyne.CanvasObject {
-			return widget.NewLabel("Cell 000, 000")
-		},
-		func(id widget.TableCellID, cell fyne.CanvasObject) {
-			label := cell.(*widget.Label)
-			switch id.Col {
-			case 0:
-				label.SetText(fmt.Sprintf("%d", id.Row+1))
-			case 1:
-				label.SetText("A longer cell")
-			default:
-				label.SetText(fmt.Sprintf("Cell %d, %d", id.Row+1, id.Col+1))
-			}
-		})
-	// t.SetColumnWidth(0, 34)
-	// t.SetColumnWidth(1, 102)
+	t.SetColumnWidth(0, 200)
+	t.SetColumnWidth(1, 1000)
 	// content
 	w.SetContent(container.NewBorder(
 		container.NewBorder(nil, nil, nil, container.NewHBox(open, openFolder), entry),
